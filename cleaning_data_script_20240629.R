404f4159aea1bf23697cf27f293eae8787934e7d
Megan Brown <brown.megan118@yahoo.com>
2024-06-29 15:48:55 -0700

    Added dotplots by fat content

--- cleaning_data_script_20240629.R
+++ cleaning_data_script_20240629.R
@@ -39,7 +39,54 @@ cheeses_new <- cheeses %>%
   left_join(., fat_content_tidy, by = join_by(cheese)) %>% 
   relocate(fat_content_new, .after = fat_content)
 
+## Create texture and fat content dataframe
+texture_pivot <- cheeses_new %>% 
+  select(cheese, texture, fat_content_new) %>% 
+  filter(!is.na(fat_content_new)) %>% 
+  filter(!is.na(texture)) %>% 
+  separate_longer_delim(texture, ", ") %>% 
+  filter(texture %in% c("smooth", "supple", "firm", "crumbly", "creamy", "soft"))
+
+
+ggplot(texture_pivot, aes(x = texture, y = fat_content_new)) +
+  geom_dotplot(binaxis = "y", stackdir = "centerwhole", dotsize = .8)
 
 
 
+## Create type and fat content dataframe
+type_pivot <- cheeses_new %>% 
+  select(cheese, type, fat_content_new) %>% 
+  filter(!is.na(fat_content_new)) %>% 
+  filter(!is.na(type)) %>% 
+  separate_longer_delim(type, ", ") %>% 
+  filter(type %in% c("hard", "semi-hard", "semi-soft", "soft"))
 
+
+ggplot(type_pivot, aes(x = type, y = fat_content_new)) +
+  geom_dotplot(binaxis = "y", stackdir = "centerwhole", dotsize = .8)
+
+
+## Create cheese family and fat content dataframe
+family_pivot <- cheeses_new %>% 
+  select(cheese, family, fat_content_new) %>% 
+  filter(!is.na(fat_content_new)) %>% 
+  filter(!is.na(family)) %>% 
+  separate_longer_delim(family, ", ") %>% 
+  filter(family %in% c("Blue", "Brie", "Cheddar", "Gouda"))
+
+
+ggplot(family_pivot, aes(x = family, y = fat_content_new)) +
+  geom_dotplot(binaxis = "y", stackdir = "centerwhole", dotsize = .8)
+
+
+## Create milk and fat content dataframe
+milk_pivot <- cheeses_new %>% 
+  select(cheese, milk, fat_content_new) %>% 
+  filter(!is.na(fat_content_new)) %>% 
+  filter(!is.na(milk)) %>% 
+  separate_longer_delim(milk, ", ") %>% 
+  filter(milk %in% c("cow", "goat", "sheep"))
+
+
+ggplot(milk_pivot, aes(x = milk, y = fat_content_new)) +
+  geom_dotplot(binaxis = "y", stackdir = "centerwhole", dotsize = .8)
